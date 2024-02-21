b386b65de161db010f2ae0343eed61ddd455da28
NightUmbrella <githubumbrella@gmail.com>
2024-02-21 19:54:42 +0900

    add logo comp

---
+++ app/(main)/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="max-w-[1400px] mx-auto">
+      <Navbar />
+    </main>
+  );
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '@/components/logo'
+
+const Navbar = () => {
+  return (
+    <div>
+        <Logo/>
+    </div>
+  )
+}
+
+export default Navbar
